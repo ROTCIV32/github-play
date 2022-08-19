9ab683a0fccf7ea90e9a6165dd6e8a038db23838
Victor Kings <kingvic2553@gmail.com>
2022-08-19 14:39:03 -0700

    Trying out

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import TopBar from "./components/topbar/TopBar";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <TopBar />
+    </div>
+  );
+
+
+}
+
+export default App;
+
+
